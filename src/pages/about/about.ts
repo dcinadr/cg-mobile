94e9eb9b666ccab4d0aabea43f9d5bd6499e39a7
David Cinadr <dcinadr@gmail.com>
2018-02-25 14:00:51 -0800

    hooked up database

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +9,24 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  coins = [];
 
+  constructor(public navCtrl: NavController) {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDG7JG_MkbMZsD1L9hAHfyiZ3VZ9nOXPFc',
+      authDomain: 'cg-mobile-4e876.firebaseapp.com',
+      projectId: 'cg-mobile-4e876'
+    });
+
+    let db = firebase.firestore();
+
+    db.collection('coins')
+      .onSnapshot(snapshot => {
+        this.coins = [];
+        snapshot.docs.forEach(doc => {
+          this.coins.push(doc.data());
+        });
+      });
   }
 
 }
