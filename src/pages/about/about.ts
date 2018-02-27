8be2a6a6804141af3151ec69d7a4784656286117
David Cinadr <dcinadr@gmail.com>
2018-02-26 21:28:02 -0800

    updated format

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -20,11 +20,16 @@ export class AboutPage {
 
     let db = firebase.firestore();
 
+    let currFormatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 });
+
     db.collection('coins')
       .onSnapshot(snapshot => {
         this.coins = [];
         snapshot.docs.forEach(doc => {
-          this.coins.push(doc.data());
+          let data = doc.data();
+          data.price_usd = currFormatter.format(data.price_usd);
+          data.point_change_24h = currFormatter.format(data.point_change_24h);
+          this.coins.push(data);
         });
       });
   }
